b839ef24183ac566644404d995365d347ffdfac4
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2018-01-02 15:47:05 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+http = require('http');
+app.use(express.static(__dirname)); // set the static files location /public/img will be /img for users
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+
+//static files 
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use(express.static(__dirname + '/public'));
