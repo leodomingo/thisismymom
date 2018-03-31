3c4539f34e83139e32ab5968696e05f2f43d2451
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2018-03-30 22:57:06 -0400

    nice

--- public/js/runner.js
+++ public/js/runner.js
@@ -1,6 +1,9 @@
 $(function() {
     const buttonArray = [$("#writing-button"), $("#code-button")];
-    const sectionArray = [$("#writing"), $("#code")];
+    const sectionArray = [$("#write-blurb"), $("#code-blurb")];
+    const items = $("img, .draggable");
+
+
     $(".draggable").draggable();
     $('.right-menu').velocity({
         properties: { scale: 1.02 },
@@ -12,13 +15,30 @@ $(function() {
         })
     });
 
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 200) {
+            scaleOut();
+        }
+    })
+
 });
 
 function scrollTo($section) {
     // Remove "link" from the ID
     // Scroll
     $('html,body').animate({
-            scrollTop: $section.offset().top
+            scrollTop: $section.offset().top - 500
         },
         'slow');
+}
+
+function scaleOut() {
+    $firstName = $(".first-name");
+    $lastName = $(".last-name");
+    $firstName.velocity({
+        properties: { width: 150, top: 15, left: 5 },
+    });
+    $lastName.velocity({
+        properties: { width: 300, top: 65, left: 5 },
+    });
 }
