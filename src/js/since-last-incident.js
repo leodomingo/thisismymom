57a04743c21cfeaaa114118bf3f5017d93c9c127
leodomingo <la5aj@virginia.edu>
2019-01-30 21:48:50 -0500

    update

--- src/js/since-last-incident.js
+++ src/js/since-last-incident.js
@@ -1,8 +1,11 @@
 $(function() {
-    $timeUI = $(".days-since-last-incident");
+    $hiredUI = $(".days-since-last-incident");
+    $graduatedUI = $(".days-until-incident");
     var hired = new Date('May 21, 2017 09:00:00');
-    var notFired = new Date();
-    days = Math.ceil(((notFired - hired) / (1000 * 60 * 60 * 24)));
-    $timeUI.text(days);
-    // console.log($timeUI);
+    var graduated = new Date('May 17, 2019 09:00:00');
+    var notFiredOrGraduated = new Date();
+    var daySinceHire = Math.ceil(((notFiredOrGraduated - hired) / (1000 * 60 * 60 * 24)));
+    var daysUntilFreedom = Math.ceil(((graduated - notFiredOrGraduated) / (1000 * 60 * 60 * 24)));
+    $hiredUI.text(daySinceHire);
+    $graduatedUI.text(daysUntilFreedom);
 });
